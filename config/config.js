db74cf7d2ed442cbc2d33be0caf78dfa843ceb43
AlexArdelean21 <ardeleanalexandru22@stud.ase.ro>
2024-12-04 17:15:54 +0200

    Successfully connected backend to MySQL database

--- config/config.js
+++ config/config.js
@@ -2,33 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgresql',
+    username: 'feedback_user', 
+    password: 'faraparola',  
+    database: 'feedback_app',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'feedback_user',
+    password: 'faraparola',
+    database: 'feedback_app',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: 'feedback_user',
+    password: 'faraparola',
+    database: 'feedback_app',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
 };
-
