1a6b0fca1cc13864826bbdeae3727876d8e38d3b
AlexArdelean21 <ardeleanalexandru22@stud.ase.ro>
2024-12-04 17:43:52 +0200

    Update Swagger configuration to include Activity and Feedback schemas and change local server URL

--- src/swagger.js
+++ src/swagger.js
@@ -10,12 +10,68 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
+        url: 'http://localhost:7000/api',
         description: 'Local development server',
       },
     ],
+    components: {
+      schemas: {
+        Activity: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'The unique identifier of the activity',
+            },
+            description: {
+              type: 'string',
+              description: 'Description of the activity',
+            },
+            accessCode: {
+              type: 'string',
+              description: 'Unique access code for the activity',
+            },
+            startTime: {
+              type: 'string',
+              format: 'date-time',
+              description: 'The start time of the activity',
+            },
+            endTime: {
+              type: 'string',
+              format: 'date-time',
+              description: 'The end time of the activity',
+            },
+          },
+          required: ['description', 'accessCode', 'startTime', 'endTime'],
+        },
+        Feedback: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'The unique identifier of the feedback',
+            },
+            emotion: {
+              type: 'string',
+              enum: ['smiley', 'frowny', 'surprised', 'confused'],
+              description: 'The emotion feedback given by a student',
+            },
+            activityId: {
+              type: 'string',
+              description: 'The ID of the activity for which the feedback is given',
+            },
+            createdAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'Timestamp when the feedback was created',
+            },
+          },
+          required: ['emotion', 'activityId'],
+        },
+      },
+    },
   },
-  apis: ['./src/routes/*.js'], // Point to route files where Swagger annotations will be added
+  apis: ['./src/routes/*.js'], 
 };
 
 const swaggerSpec = swaggerJSDoc(options);
