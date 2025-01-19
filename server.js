25dd32a812a0550bbe3ba962b7d216ce58e4c979
Ana Boncu <anaboncu@MacBook-Air-Ana.local>
2025-01-19 15:10:36 +0200

    Updated CORS to allow Vercel frontend

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://feedback-app-ruddy.vercel.app/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
