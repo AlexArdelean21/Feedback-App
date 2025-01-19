d9336f689731301a0621933f3214ec0791401387
Ana Boncu <anaboncu@MacBook-Air-Ana.local>
2025-01-19 16:01:58 +0200

    Updated CORS to allow all origins for testing

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://feedback-app-ruddy.vercel.app/'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+  origin: '*', // Allow all origins
 }));
+
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
